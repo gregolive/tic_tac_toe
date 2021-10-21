49be6ab70d382701583aaa6b24edeec09d7c7eb6
Gregory Olive <gregoryolive@outlook.com>
2021-10-21 19:17:31 +0900

    Added further rspec testing

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -5,7 +5,7 @@ require_relative '../logic'
 require_relative '../messages'
 
 describe Game do
-  # 1. Make sure players win when they should
+  # Make sure players win when they should
   subject(:game_over) { described_class.new }
 
   context "when a player gets 3 in a row" do
@@ -31,8 +31,52 @@ describe Game do
     end
   end
 
-  # 1. Test each critical method to make sure it functions properly and handles edge cases
-  subject(:game_critical) { described_class.new }
+  # Only allow proper marker input
+  subject(:in_game) { described_class.new }
 
-  
+  context 'when marker is entered' do
+    describe '#length_check' do
+      it 'should return false if the marker is one character' do
+        marker = 'F'
+        check = in_game.length_check(marker)
+        expect(check).to be_falsey
+      end
+
+      it 'should return true if the marker is more than one character' do
+        marker = 'FB'
+        check = in_game.length_check(marker)
+        expect(check).to be_truthy
+      end
+    end
+    
+    describe '#number_check' do
+      it 'should return false if the marker is a non-number' do
+        marker = 'F'
+        check = in_game.number_check(marker)
+        expect(check).to be_falsey
+      end
+
+      it 'should return true if the marker is a number' do
+        marker = '1'
+        check = in_game.number_check(marker)
+        expect(check).to be_truthy
+      end
+    end
+
+    describe '#check_duplicate' do
+      it 'should return false if the marker is not used by player 1' do
+        p1_marker = 'G'
+        marker = 'F'
+        check = in_game.check_duplicate(marker, p1_marker)
+        expect(check).to be_falsey
+      end
+
+      it 'should return true if the marker is used by player 1' do
+        p1_marker = 'F'
+        marker = 'F'
+        check = in_game.check_duplicate(marker, p1_marker)
+        expect(check).to be_truthy
+      end
+    end
+  end
 end
